2ed093171e1b1bcf044c522c7d939034578868d0
hi1d <aopd408@gmail.com>
2022-01-24 23:01:23 +0900

    [Django]post_detail_page_tdd

--- do_it_django/blog/tests.py
+++ do_it_django/blog/tests.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from .models import Post
@@ -52,3 +53,35 @@ class TestView(TestCase):
 
         # not "Post is Empty"
         self.assertNotIn('Post is Empty.', main_area.text)
+
+    def test_post_detail(self):
+        # if post
+        post_001 = Post.objects.create(
+            title='First Post',
+            content='Hello, World. We are the World.'
+        )
+        self.assertEqual(Post.objects.count(), 1)
+
+        # post_url == '/blog/1'
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # response.status_code == 200
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+        # navbar
+        soup = BeautifulSoup(response.content, 'html.parser')
+        navbar = soup.nav
+        self.assertIn('Home', navbar.text)
+        self.assertIn('About', navbar.text)
+
+        # post_title in Web Tab Title
+        self.assertIn(post_001.title, soup.title)
+
+        # post_title in Post-area
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        # post_content in post-area
+        self.assertIn(post_001.content, post_area.text)
